6c7479f4b31a26dd2af23df3ead982056c7e57fd
Christopher Diaz <pocketofconflict@gmail.com>
2021-04-06 03:30:03 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,8 +1,7 @@
 function basicTeenager(age) {
-  if (age >= 13 && age <= 19) {
-    console.log('You are a teenager')
+  if (age >= 13 && age <=19) {
+    console.log("You are a teenager")
   }
-
 }
 
 function teenager(age) {
