d9c5a32314dd487fc403a1c836f5b39f8acdb30a
Christopher Diaz <pocketofconflict@gmail.com>
2021-04-06 04:10:56 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -13,6 +13,8 @@ else
 function ageChecker(age) {
   if (age >= 13 && age <=19)
     return("You are a teenager!")
+    else if (age <= 12)
+    return("You are a kid")
 }
 
 function ternaryTeenager(age) {
